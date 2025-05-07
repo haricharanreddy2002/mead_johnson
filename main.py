dae6c5f39c0f5a7c49334c2d16b55c70d6719edf
haricharanreddy2002 <HariCharan@chervic.in>
2025-05-07 15:02:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -449,4 +449,4 @@ async def handle_query(value: str):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.1", port=8000)
